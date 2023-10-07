7360a61b02353c24cbeb4eeb08f12d625b740435
Enzo Reboredo <enzooreboredo@gmail.com>
2023-10-07 15:15:11 -0300

    add more topics

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,28 +9,18 @@ const About = () => {
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            About
+            About Me
           </p>
         </div>
 
         <p className="text-xl mt-20">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-          iste, totam fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
+        My name is Enzo Reboredo, I am 20 years old and I am studying the last year of the University Technician of Development of Computer Applications in Argentina, located in Tandil, Buenos Aires.Currently studying and working in a restaurant and party hall, eager to work in what I really like.
         </p>
 
         <br />
 
         <p className="text-xl">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          pariatur, vel similique sint, nobis aspernatur ut praesentium
-          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
-          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-          magni quo! Eum cupiditate debitis labore.
+        I have a great goal, to dedicate myself and focus on being a good web developer. Always since I was a child I was very creative, and very curious, because of that, it made me interested in technology and passionate about this beautiful world that does not stop progressing, fortunately now I study to do what I really like to program, develop and design.
         </p>
       </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
     <div name="Home" className='h-screen w-full bg-gradient-to-b from-black  via-black to-gray-800'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row '>
             <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-4xl  sm:text-7xl font-bold text-white'>Im a Web Developer</h2>
-                <p className='text-gray-500 py-4 max-w-md'>I have knowledge and experience, building and designing software. Currently, yo amo to work on web application using technologies like JavaScript, Java, React, Angular.</p>
+                <h2 className='text-4xl  sm:text-7xl font-bold text-white'>I'm a Software Developer</h2>
+                <p className='text-gray-500 py-4 max-w-md'>I have knowledge and experience, building and designing software. Currently, I love to work on web application using technologies like JavaScript, Java, React, Angular.</p>
                 <div>
                     <Link to='Portfolio' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer '>
                          Portfolio
